59411d33d62734d875becb34731e7a42f81641a6
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2022-05-01 17:36:33 -0600

    CSV Reports

---
+++ bin/full_console.dart
@@ -0,0 +1,29 @@
+import "dart:io";
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty){
+    print('Usage; dart full-console.dart <export.csv>');
+    exit(1);
+  }
+  final inputFile = arguments.first;
+  final lines = File(inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double> {};
+  lines.removeAt(0); //Removes header line
+  for (var line in lines){
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    final tag = values[5].replaceAll('"','');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null){
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] = previousTotal + duration;
+    }
+  }
+  for (var entry in totalDurationByTag.entries){
+    final durationFormatted = entry.value.toStringAsFixed(1);
+    final tag = entry.key;
+    print('$tag: ${durationFormatted}h');
+  }
+}
